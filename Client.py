4ddcd89456669bea4539aef6c81534aee4d1adbb
firasabs <firasabosada@gmail.com>
2024-10-07 23:09:22 +0300

    update Server.py

--- Client.py
+++ Client.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import socket
 Server_ip  = '127.0.0.1'
 Server_Port = 5050
@@ -9,3 +10,13 @@ client_socket.send(message.encode())  # Encode the message to bytes before sendi
 #response = client_socket.recv(1024)  # Receive data from server (buffer size of 1024 bytes)
 #print(f"Response from server: {response.decode('utf-8')}")  # Decode and print server response
 client_socket.close() #close the connection
+=======
+import socket
+serverIp = '127.0.0.1'
+serverPort = 5050
+# Simple client setup
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((serverIp, serverPort))  # Connect to the server
+client_socket.send("Hello, Server its me the client".encode('utf-8'))  # Send a simple message
+client_socket.close()  # Close the connection
+>>>>>>> 50637bf (update Server.py)
