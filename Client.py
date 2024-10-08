ca3ff3768b4de0d8ce03e2bb14de29acce9ab985
firasabs <firasabosada@gmail.com>
2024-10-09 00:21:04 +0300

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import socket
 serverIp = '127.0.0.1'
 serverPort = 5050
@@ -7,13 +6,3 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((serverIp, serverPort))  # Connect to the server
 client_socket.send("Hello, Server its me the client".encode('utf-8'))  # Send a simple message
 client_socket.close()  # Close the connection
-=======
-import socket
-serverIp = '127.0.0.1'
-serverPort = 5050
-# Simple client setup
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((serverIp, serverPort))  # Connect to the server
-client_socket.send("Hello, Server its me the client".encode('utf-8'))  # Send a simple message
-client_socket.close()  # Close the connection
->>>>>>> firasB
