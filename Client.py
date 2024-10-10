bd04de2c715173b44ce224381d17182560f11fdf
firasabs <firasabosada@gmail.com>
2024-10-10 13:40:47 +0300

    deleting Client.py

--- Client.py
+++
@@ -1,8 +0,0 @@
-import socket
-serverIp = '127.0.0.1'
-serverPort = 12345
-# Simple client setup
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((serverIp, serverPort))  # Connect to the server
-client_socket.send("hello server, from client ".encode('utf-8'))  # Send a simple message
-client_socket.close()  # Close the connection
